dc7e515971ddcd2796ced3e42d9ef33af7dd0217
yaochong-cc <147566535+yaochong-cc@users.noreply.github.com>
2024-04-28 00:15:00 +0800

    Add files via upload

--- Rotate Array.cpp
+++ Rotate Array.cpp
@@ -3,22 +3,53 @@
 //
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
+//class Solution {
+//public:
+//    void rotate (vector<int> &nums ,int k){
+//        vector<int> res(nums.size());//一定要规定数组长度：
+//        for(int i=0;i<nums.size();i++){
+//            res[(i+k)%nums.size()]=nums[i];
+//        }
+//        for(int i=0;i<nums.size();i++){
+//            nums[i]=res[i];
+//        }
+//    }
+//};
+
+//// 数组翻转
+//class Solution {
+//public:
+//    void rotate (vector<int> &nums ,int k){
+//        int n=nums.size();
+//        for(int start=0,cnt=0;cnt<n;start++){
+//            int cur=start;
+//            int temp=nums[cur];
+//            do{
+//                int next =(cur+k)%n;
+//                swap(nums[next],temp);
+//                cur=next;
+//                cnt++;
+//            }while(cur!=start);
+//        }
+//    }
+//};
+
+//翻转再翻转
 class Solution {
 public:
     void rotate (vector<int> &nums ,int k){
-        vector<int> res(nums.size());//一定要规定数组长度：
-        for(int i=0;i<nums.size();i++){
-//            if(i+k<=nums.size()) res[i+k-1]=nums[i];
-//            else if(i+k>nums.size()) res[i+k-nums.size()]=nums[i];
-            res[(i+k)%nums.size()]=nums[i];
-        }
-        for(int i=0;i<nums.size();i++){
-            nums[i]=res[i];
-        }
+        int n =nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k%n);
+        reverse(nums.begin()+k%n,nums.end());
     }
 };
+
+
+
 int main(){
     Solution sol;
     vector<int> nums={1,2,3,4,5,6,7};
